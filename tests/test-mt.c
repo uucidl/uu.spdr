5a29687ee828b85c65acd71431dbdec9898d5e01
Nicolas Léveillé <nicolas@uucidl.com>
2012-09-30 18:22:17 +0200

    Transform traces into chrome json
    The files produced by tojson.pl can be directly
    loaded by the chrome://tracing console.
    
    Use with:
    $ ./mt-with-tracing 2> trace.txt
    Hello, 世界.
    $ perl tojson.pl trace.txt > chrome.json
    
    which can be loaded in chrome.

--- tests/test-mt.c
+++ tests/test-mt.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <math.h>
 
 #include "spdr.h"
 
@@ -23,10 +24,26 @@ void trace (const char* line)
 
 void* thread1(void* arg)
 {
-	SPDR_BEGIN1(spdr, "Main", "thread1", SPDR_INT("arg", (int) arg));
+	int n = 30;
+	double x = 0.1234;
+	double y = 0.117;
 
-	SPDR_END(spdr, "Main", "thread1");
+	while (n--) {
+		int cosn = 16*65536;
+		int pown = 32*65536;
 
+		SPDR_BEGIN2(spdr, "Main", "thread1", SPDR_INT("arg", (int) arg), SPDR_FLOAT("y", y));
+		while (cosn--) {
+			x += cos(x);
+		}
+		SPDR_END(spdr, "Main", "thread1");
+
+		while (pown--) {
+			y += atan2(y, x);
+		}
+	}
+
+	pthread_exit(NULL);
 	return NULL;
 }
 
@@ -36,7 +53,8 @@ int main (int argc, char** argv)
 	spdr_enable_trace(spdr, TRACING_ENABLED);
 	spdr_set_log_fn(spdr, trace);
 
-	SPDR_BEGIN(spdr, "Main", "main");
+	SPDR_BEGIN2(spdr, "Main", "main",
+		    SPDR_INT("argc", argc), SPDR_STR("argv[0]", argv[0]));
 	pthread_t thread;
 	pthread_create(&thread, NULL, thread1, NULL);
 
@@ -44,8 +62,10 @@ int main (int argc, char** argv)
 	sleep (3);
 	printf (" 世界.\n");
 
+	SPDR_BEGIN(spdr, "Main", "Waiting For Thread1");
 	void* status;
-	pthread_join (thread, status);
+	pthread_join (thread, &status);
+	SPDR_END(spdr, "Main", "Waiting For Thread1");
 
 	SPDR_END(spdr, "Main", "main");
 	pthread_exit (NULL);
---
+++ tests/tojson.pl
@@ -0,0 +1,37 @@
+#! env perl
+
+$quoted_str_re = /\"([^\\\"]|\\.)*\"/;
+$prefix = "";
+
+printf "{\"traceEvents\":[";
+
+while (<>) {
+    if (/^([0-9]+) ([0-9]+) ([0-9]+) (\"(?:[^\\\"]|\\.)*\") (\"(?:[^\\\"]|\\.)*\") (\"[A-Z]\")(.*)$/) {
+	printf $prefix;
+	printf "{\"ts\":" . $1;
+	printf ",\"pid\":" . $2;
+	printf ",\"tid\":" . $3;
+	printf ",\"cat\":" . $4;
+	printf ",\"name\":" . $5;
+	printf ",\"ph\":" . $6;
+	printf ",\"args\":{";
+
+	$args = $7;
+	$subprefix = "";
+	while ($args =~ / (\"(?:[^\\\"]|\\.)*\") ((?:\"(?:[^\\\"]|\\.)*\")|(?:[0-9]+\.[0-9]*)|(?:[0-9]+))/g) {
+	    printf "$subprefix$1:$2";
+	    $subprefix = ",";
+	}
+
+	    # for (i=7 ; i<= NF; i+=2) {
+	#	if ($7 ~ /".*"/) {
+	#		printf subprefix $i ": " $(i+1)
+	#		subprefix = ","
+	#	}
+	# }
+	printf "}}";
+	$prefix = ",";
+   }
+}
+
+printf "]}";
