61272180c963f9347de0ca7294506b6c53f5c853
Nicolas <nicolas@uucidl.com>
2012-11-09 08:32:46 +0100

    stdint.h for msvc
    Microsoft is now shipping stdint.h so only use the replacement for
    versions of visual studio below VS 2010.

--- src/uu-stdint.h
+++ src/uu-stdint.h
@@ -1,7 +1,8 @@
 #ifndef UU_SPDR_STDINT_H
 #define UU_SPDR_STDINT_H
 
-#if defined(_MSC_VER)
+#if defined(_MSC_VER) < 1600
+/* Before VS 2010, Microsoft did not ship a stdint.h */
 #include "stdint-msvc.h"
 #else
 #include <stdint.h>
