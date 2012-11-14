74222b9eead66ab70a57f124aaf96f741637fa89
Nicolas Léveillé <nicolas@uucidl.com>
2012-11-14 09:57:58 +0100

    fixup!    stdint.h for msvc

--- src/uu-stdint.h
+++ src/uu-stdint.h
@@ -1,7 +1,7 @@
 #ifndef UU_SPDR_STDINT_H
 #define UU_SPDR_STDINT_H
 
-#if defined(_MSC_VER) < 1600
+#if defined(_MSC_VER) && _MSC_VER < 1600
 /* Before VS 2010, Microsoft did not ship a stdint.h */
 #include "stdint-msvc.h"
 #else
