114589626f2e5517fb28943e2832f2b5668b327b
Nicolas <nicolas@uucidl.com>
2012-11-25 18:06:54 +0100

    linux compilation and implementation

--- src/clock-posix.c
+++ src/clock-posix.c
@@ -9,7 +9,15 @@ struct Clock
 
 extern int clock_init (struct Clock** clockp, struct Allocator* allocator)
 {
+  struct timespec res = { 0 };
+
   (void) allocator;
+  
+  if (clock_getres(CLOCK_MONOTONIC, &res) != 0 || 
+      res.tv_nsec > 1000) {
+    /* unknown clock or insufficient resolution */
+    return -1;
+  }
 
   *clockp = NULL; 
 
@@ -23,7 +31,7 @@ extern void clock_deinit(struct Clock** clockp)
 
 extern uint64_t clock_microseconds(struct Clock* clock)
 {
-  struct timespec ts;
+  struct timespec ts = { 0 };
   clock_gettime(CLOCK_MONOTONIC, &ts);
 
   return ts.tv_nsec / 1000;
