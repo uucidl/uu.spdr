5424055888d98d5128bcafc3755a5c85584f4d72
Nicolas Léveillé <nicolas@uucidl.com>
2015-09-20 13:19:13 +0200

    D should be visible to printf

--- build_examples_posix.sh
+++ build_examples_posix.sh
@@ -38,30 +38,30 @@ EXAMPLES="${HERE}"/examples
 # here is all you need:
 SPDR=(-I"${HERE}"/include "${HERE}"/src/spdr_posix_unit.c -lrt)
 
-D="${OUTPUT}"/test \
- "${CC}" "${cflags[@]}" -ansi "${EXAMPLES}"/test.c -lm "${SPDR[@]}" \
- -o "${D}" \
-    && printf "PROGRAM\t%s" "${D}"
+D="${OUTPUT}"/test
+"${CC}" "${cflags[@]}" -ansi "${EXAMPLES}"/test.c -lm "${SPDR[@]}" \
+        -o "${D}"
+printf "PROGRAM\t%s" "${D}"
 
-D="${OUTPUT}"/test-scope \
- "${CC}" "${cflags[@]}" "${EXAMPLES}"/test-scope.c -lm "${SPDR[@]}" \
- -o "${D}" \
-    && printf "PROGRAM\t%s" "${D}"
+D="${OUTPUT}"/test-scope
+"${CC}" "${cflags[@]}" "${EXAMPLES}"/test-scope.c -lm "${SPDR[@]}" \
+        -o "${D}"
+printf "PROGRAM\t%s" "${D}"
 
-D="${OUTPUT}"/test-mt \
- "${CC}" "${cflags[@]}" -ansi "${EXAMPLES}"/test-mt.c -lm "${SPDR[@]}" \
- -o "${D}" \
-    && printf "PROGRAM\t%s" "${D}"
+D="${OUTPUT}"/test-mt
+"${CC}" "${cflags[@]}" -ansi "${EXAMPLES}"/test-mt.c -lm "${SPDR[@]}" \
+        -o "${D}"
+printf "PROGRAM\t%s" "${D}"
 
-D="${OUTPUT}"/test-full \
- "${CC}" "${cflags[@]}" -ansi "${EXAMPLES}"/test-full.c -lm "${SPDR[@]}" \
- -o "${D}" \
-    && printf "PROGRAM\t%s" "${D}"
+D="${OUTPUT}"/test-full
+"${CC}" "${cflags[@]}" -ansi "${EXAMPLES}"/test-full.c -lm "${SPDR[@]}" \
+        -o "${D}"
+printf "PROGRAM\t%s" "${D}"
 
-D="${OUTPUT}"/test-cxx \
- "${CXX}" "${cflags[@]}" "${EXAMPLES}"/test-cxx.cc -lm "${SPDR[@]}" \
- -o "${D}" \
-    && printf "PROGRAM\t%s" "${D}"
+D="${OUTPUT}"/test-cxx
+"${CXX}" "${cflags[@]}" "${EXAMPLES}"/test-cxx.cc -lm "${SPDR[@]}" \
+         -o "${D}"
+printf "PROGRAM\t%s" "${D}"
 
 ## ..BUILD EXAMPLES>
 
