b8481b3fac6f286d22ac890deedf3f3993229c38
Nicolas <nicolas@uucidl.com>
2024-05-23 22:01:53 +0200

    Add _POSIX_C_SOURCE to linux unit.
    This is helpful when passing -std=c99

--- src/spdr_linux_unit.c
+++ src/spdr_linux_unit.c
@@ -2,15 +2,31 @@
 extern "C" {
 #endif
 
+#if _POSIX_C_SOURCE < 200112L
+#define _POSIX_C_SOURCE 200112L
+
+#ifndef _POSIX_C_SOURCE
+#define SPDR_POSIX_UNIT_DEFINED_POSIX_C_SOURCE (1)
+#endif
+
+#endif
+
+/* generic modern POSIX platforms */
+
 #include "allocator.c"
-#include "chars_posix.c"
 #include "chars.c"
-#include "clock_posix.c"
+#include "chars_posix.c"
 #include "clock.c"
+#include "clock_posix.c"
 #include "float_posix.c"
-#include "spdr_posix.c"
 #include "spdr-thread_posix.c"
 #include "spdr.c"
+#include "spdr_posix.c"
+
+#if SPDR_POSIX_UNIT_DEFINED_POSIX_C_SOURCE
+#undef _POSIX_C_SOURCE
+#undef SPDR_POSIX_UNIT_DEFINED_POSIX_C_SOURCE
+#endif
 
 #ifdef __cplusplus
 }
