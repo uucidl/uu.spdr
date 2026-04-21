41beebf59748ae0329dfe114163ebdd6dc6b4637
Nicolas <nicolas@uucidl.com>
2026-04-21 09:23:31 +0900

    Add test case for MSVC preprocessing.

--- build_tests.bat
+++ build_tests.bat
@@ -32,8 +32,28 @@ echo TEST	%D%
 %D%
 if %errorlevel% neq 0 goto error_end_test
 
+REM Test that spdr can pass preprocessing:
+REM
+REM preprocess using -P and check if it compiles
+REM
+REM recent libatomic_ops include code like unsigned/**/char
+REM which trips the MSVC preprocessor, as it does not appear
+REM to follow the standard, and does not generate a space in that case.
+REM
+set D=%BUILD%\preprocessed.i
+cl.exe -Fi:%D% ^
+  -P ^
+  %CLFLAGS% %TOP%\tests\spdr_basic_tests.c
+if %errorlevel% neq 0 goto error_end_build
+cl.exe -nologo -Zs -Tc %BUILD%\preprocessed.i
+if %errorlevel% neq 0 goto error_end_build
+echo TEST	%D%
+
 endlocal
 exit /b 0
+
+REM Error cases
+REM ===========
 
 :error_end_nocompiler
 echo ERROR: CL.EXE missing. Have you run vcvarsall.bat?
@@ -41,7 +61,7 @@ endlocal
 exit /b 1
 
 :error_end_build
-echo ERROR: Failed to build
+echo ERROR: Failed to build %D%
 endlocal
 exit /b %errorlevel%
 
